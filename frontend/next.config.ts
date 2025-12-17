de39db141a081377bcea58eed06eda92544454d2
Kapil Tulsan <kapiltulsan@gmail.com>
2025-12-17 15:30:42 +0530

    feat: Introduce Next.js frontend with API proxy configuration and enable ProxyFix in Flask backend.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://127.0.0.1:5000/api/:path*',
+        destination: 'http://127.0.0.1:5000/api/:path*',
       },
     ];
   },
